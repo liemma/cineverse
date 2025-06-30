03a6ebcca0c13068280909cc7fcc0a33501022ea
liemma <lemma75@gmail.com>
2025-06-30 18:44:11 -0500

    Added trending

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,22 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "react-native";
 import './globals.css';
 
 export default function RootLayout() {
   // Hides group route header
-  return <Stack>
-    <Stack.Screen
-      name="(tabs)"
-      options={{ headerShown: false }}
-      />
-      <Stack.Screen
-      name="movie/[id]"
-      options={{ headerShown: false }}
-    />
-  </Stack>;
+  return (
+    <>
+      <StatusBar hidden={true}/>
+      <Stack>
+        <Stack.Screen
+          name="(tabs)"
+          options={{ headerShown: false }}
+          />
+          <Stack.Screen
+          name="movie/[id]"
+          options={{ headerShown: false }}
+        />
+      </Stack>
+    </>
+  );
 }
